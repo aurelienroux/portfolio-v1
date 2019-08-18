c4aa5ba97608cba66441dd9eafe90843cf8ba9c5
Aurélien Roux <aureroux13@me.com>
2019-08-18 11:51:24 -0400

    update index and About component

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 const About = () => (
   <div>
-    About text goes here
+    <p>
+      I'm a french canadian developer based in Montreal QC. 
+      I started development with HTML5 and CSS3 as a hobby in 2013 and after doing some websites for fun, it became my full-time job in 2016.
+    </p>
+    <p>
+      In 2017, I completed <a href="https://concordiabootcamps.ca/" target="_blank">DecodeMtl</a>, a Full-stack immersive bootcamp specializing in javascript,
+      where I have learned fundamentals of programming and how to develop websites and web applications.
+    </p>
+    <p>
+      A Lifelong learner, I’m always trying to improve my skills and learn new technologies.
+      My interests are in React, VueJs, Gatsby, NextJS and basically everything that has to do with JavaScript. 
+      I'm also very interested in back-end dev and would love to learn more about it.
+    </p>
+    <p>
+      here's a few techs that I have been working with
+    </p>
+    <ul>
+      <li>HTML & (S)CSS</li>
+      <li>Javascript es6+</li>
+      <li>React</li>
+      <li>Redux</li>
+      <li>WordPress</li>
+      <li>WooCommerce</li>
+      <li>jQuery</li>
+    </ul>
   </div>
 );
 
