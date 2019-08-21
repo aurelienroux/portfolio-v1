77e18ee2c3bd1d200a25f3388a9a095b2cf5b636
Aurélien Roux <aureroux13@gmail.com>
2019-08-20 20:57:04 -0400

    add icons to contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,50 @@
 import React from 'react';
+import { 
+  FaLinkedin, 
+  FaInstagram, 
+  FaCodepen, 
+  FaGithub 
+} from 'react-icons/fa';
 
 const Contact = () => (
   <div className="contact section">
     <h3 className="title">Contact</h3>
+    <div>
+      <a 
+        target="_blank" 
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/rouxaurelien/"
+      >
+        <FaLinkedin className="social-icon" />
+      </a>
+      <a 
+        target="_blank" 
+        rel="noopener noreferrer"
+        href="https://www.instagram.com/rxaure/"
+      >
+        <FaInstagram className="social-icon" />
+      </a>
+      <a 
+        target="_blank" 
+        rel="noopener noreferrer"
+        href="https://codepen.io/Aureroux"
+      >
+        <FaCodepen className="social-icon" />
+      </a>
+      <a 
+        target="_blank" 
+        rel="noopener noreferrer"
+        href="https://github.com/aurelienroux"
+      >
+        <FaGithub className="social-icon" />
+      </a>
+    </div>
+    <a 
+      className="button"
+      href="mailto:aureroux13@gmail.com"
+    >
+      Keep in touch
+    </a>
   </div>
 );
 
--- src/components/HeaderHero.js
+++ src/components/HeaderHero.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FaReact } from 'react-icons/fa';
-import { DiNodejsSmall } from 'react-icons/di';
 
 const HeaderHero = () => (
   <div className="header section">
@@ -8,7 +6,7 @@ const HeaderHero = () => (
     <h1 className="header__title">Aurelien Roux</h1>
     <h2 className="header__sub">Building things for the web</h2>
     <p className="header__intro">
-      I'm a Front-End developer based in Montreal, <FaReact />  <DiNodejsSmall />
+      I'm a Front-End developer based in Montreal,
       who likes to create engaging websites and amazing web applications
     </p>
   </div>
