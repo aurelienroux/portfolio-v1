57eb24c4cd0cc3ab3c03fd9c358cd32d21b0f832
Aurélien Roux <aureroux13@me.com>
2019-08-21 21:07:24 -0400

    add icons and links to project units

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,32 +10,16 @@ const Contact = () => (
   <div className="contact section">
     <h3 className="title">Contact</h3>
     <div>
-      <a 
-        target="_blank" 
-        rel="noopener noreferrer"
-        href="https://www.linkedin.com/in/rouxaurelien/"
-      >
+      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rouxaurelien/">
         <FaLinkedin className="social-icon" />
       </a>
-      <a 
-        target="_blank" 
-        rel="noopener noreferrer"
-        href="https://www.instagram.com/rxaure/"
-      >
+      <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/rxaure/">
         <FaInstagram className="social-icon" />
       </a>
-      <a 
-        target="_blank" 
-        rel="noopener noreferrer"
-        href="https://codepen.io/Aureroux"
-      >
+      <a target="_blank" rel="noopener noreferrer" href="https://codepen.io/Aureroux">
         <FaCodepen className="social-icon" />
       </a>
-      <a 
-        target="_blank" 
-        rel="noopener noreferrer"
-        href="https://github.com/aurelienroux"
-      >
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/aurelienroux">
         <FaGithub className="social-icon" />
       </a>
     </div>
--- src/components/ProjectUnit.js
+++ src/components/ProjectUnit.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { 
+  FaExternalLinkAlt,
+  FaGithub
+} from 'react-icons/fa';
 
 const ProjectUnit = (props) => (
   <div className="project-unit">
     <h3 className="project-unit__title">{props.el.title}</h3>
+    <a target="_blank" rel="noopener noreferrer" href={props.el.url}>
+      <FaExternalLinkAlt />
+    </a>
+    {props.el.gitRepo && 
+      <a target="_blank" rel="noopener noreferrer" href={props.el.gitRepo}>
+        <FaGithub />
+      </a>
+    }
     <p>{props.el.content}</p>
-    <p>{props.el.url}</p>
-    <p>{props.el.gitRepo}</p>
     {props.el.techs.map((el) => (`${el}, `))}
   </div>
 );
