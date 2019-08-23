d52998422f8717118a45559f3114f02c73b599e9
Aurélien Roux <aroux@clever-age.com>
2019-08-23 15:58:17 -0400

    navigation component

---
+++ src/routes/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {FaLinkedin} from 'react-icons/fa';
+
+const Nav = () => (
+  <div className="navigation">
+    <a href="/home">
+      <FaLinkedin />
+    </a>
+    <a href="/home">
+      <FaLinkedin />
+    </a>
+    <a href="/home">
+      <FaLinkedin />
+    </a>
+    <a href="/home">
+      <FaLinkedin />
+    </a>
+  </div>
+);
+
+export default Nav;
