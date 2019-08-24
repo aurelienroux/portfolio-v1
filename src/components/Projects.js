bba3e96328984f32d6c552964621d78be0ee2d56
Aurélien Roux <aroux@clever-age.com>
2019-08-24 13:55:06 -0400

    bg image on project route

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,9 +3,12 @@ import dataProjects from '../data/dataProjects';
 import ProjectUnit from './ProjectUnit'
 
 const Projects = () => (
-  <div className="projects section content-container">
-    <h3 className="title">Projects</h3>
-    <div className="projects__content">
+  <div className="projects section">
+    <div className="title">
+      <div className="title__image"></div>
+      <h3 className="title__text">Projects</h3>
+    </div>
+    <div className="projects__content content-container">
       {dataProjects.map((el, index) => (
         <ProjectUnit key={index} el={el} />
       ))}
