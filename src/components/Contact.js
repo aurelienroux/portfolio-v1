ab27fcd634b7bcdbe78a9c3baaad2d1e79a593b6
Aurélien Roux <aroux@clever-age.com>
2019-08-24 14:32:00 -0400

    fix nav z-index

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,31 +7,33 @@ import {
 } from 'react-icons/fa';
 
 const Contact = () => (
-  <div className="contact section content-container">
+  <div className="contact section">
     <div className="title">
       <div className="title__image"></div>
       <h3 className="title__text">Contact</h3>
     </div>
-    <div>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rouxaurelien/">
-        <FaLinkedin className="social-icon" />
-      </a>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/rxaure/">
-        <FaInstagram className="social-icon" />
-      </a>
-      <a target="_blank" rel="noopener noreferrer" href="https://codepen.io/Aureroux">
-        <FaCodepen className="social-icon" />
-      </a>
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/aurelienroux">
-        <FaGithub className="social-icon" />
+    <div className="content-container">
+      <div>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rouxaurelien/">
+          <FaLinkedin className="social-icon" />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/rxaure/">
+          <FaInstagram className="social-icon" />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://codepen.io/Aureroux">
+          <FaCodepen className="social-icon" />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/aurelienroux">
+          <FaGithub className="social-icon" />
+        </a>
+      </div>
+      <a 
+        className="button"
+        href="mailto:aureroux13@gmail.com"
+      >
+        Keep in touch
       </a>
     </div>
-    <a 
-      className="button"
-      href="mailto:aureroux13@gmail.com"
-    >
-      Keep in touch
-    </a>
   </div>
 );
 
