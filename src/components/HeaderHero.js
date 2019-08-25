ae63ed8b4011ce57f7ea96d8f1b125ee5a403b6d
Aurélien Roux <aroux@clever-age.com>
2019-08-25 10:14:25 -0400

    responsive media queries and header resp style

--- src/components/HeaderHero.js
+++ src/components/HeaderHero.js
@@ -5,7 +5,7 @@ const HeaderHero = () => (
     <p className="header__text">Hello, I'm</p>
     <h1 className="header__title">Aurelien Roux</h1>
     <h2 className="header__sub">Building things for the web</h2>
-    <p className="header__text">
+    <p className="header__text header__text--second">
       I'm a Front-End developer based in Montreal,
       who likes to create engaging websites and amazing web applications
     </p>
