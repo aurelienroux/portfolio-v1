5bd2310700729e54fed9d88ec6d15d37c7addc84
Loïc Fuentes <loic.fuentes@aerial.ai>
2019-08-27 10:25:52 -0400

    refacto social element in contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,73 +1,52 @@
 import React from 'react';
-import { 
-  FaLinkedin,
-  FaInstagram,
-  FaCodepen,
-  FaGithub
-} from 'react-icons/fa';
+import { FaCodepen, FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
+
 import CV from '../images/CV-AR.pdf';
 
-const Contact = () => (
-  <div className="contact section">
-    <div className="title">
-      <div className="title__image"></div>
-      <h3 className="title__text">Contact</h3>
-    </div>
-    <div className="content-container">
-      <p className="headline">
-        An idea you want to share ? <br /> 
-        Just want to say hi ? <br /> 
-        Let's talk!
-      </p>
-      <div className="social__container">
-        <div className="social">
-          <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rouxaurelien/">
-            <FaLinkedin className="social__icon" />
+const Contact = () => {
+  return (
+    <div className="contact section">
+      <div className="title">
+        <div className="title__image"></div>
+        <h3 className="title__text">Contact</h3>
+      </div>
+      <div className="content-container">
+        <p className="headline">
+          An idea you want to share?<br /> 
+          Just want to say hi?<br /> 
+          Let's talk!
+        </p>
+        <div className="social__container">
+          <Social FaIcon={FaLinkedin} link="https://www.linkedin.com/in/rouxaurelien/" />
+          <Social FaIcon={FaInstagram} link="https://www.instagram.com/rxaure/"/>
+          <Social FaIcon={FaCodepen} link="https://codepen.io/Aureroux"/>
+          <Social FaIcon={FaGithub}  link="https://github.com/aurelienroux" />
+        </div>
+  
+        <div className="buttons__container">
+          <a className="button" href="mailto:aureroux13@gmail.com">
+            Say hi
           </a>
-          <div className="social__line"></div>
+          
+          <a className="button" href={CV} rel="noopener noreferrer" target="_blank">
+            Resume
+          </a>
         </div>
         
-        <div className="social">
-          <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/rxaure/">
-            <FaInstagram className="social__icon" />
-          </a>
-          <div className="social__line"></div>
-        </div>
-
-        <div className="social">
-          <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://codepen.io/Aureroux">
-            <FaCodepen className="social__icon" />
-          </a>
-          <div className="social__line"></div>
-        </div>
-
-        <div className="social">
-          <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://github.com/aurelienroux">
-            <FaGithub className="social__icon" />
-          </a>
-          <div className="social__line"></div>
-        </div>
       </div>
-
-      <div className="buttons__container">
-        <a 
-          className="button"
-          href="mailto:aureroux13@gmail.com"
-        >
-          Say hi
-        </a>
-        <a 
-          target="_blank"
-          rel="noopener noreferrer"
-          className="button"
-          href={CV}
-        >
-          Resume
-        </a>
-      </div>
-      
     </div>
-  </div>
-);
+  )
+};
+
+const Social = ({ FaIcon, href }) => {
+  return (
+    <div className="social" key={href}>
+      <a className="social__link" href={href} rel="noopener noreferrer" target="_blank" >
+        <FaIcon className="social__icon" />
+      </a>
+      <div className="social__line"></div>
+    </div>
+  )
+};
 
 export default Contact;
