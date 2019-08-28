8128616708799b1e150507160c2fcae6f286bf10
Aurélien Roux <aureroux13@me.com>
2019-08-28 19:57:15 -0400

    refacto home scss

--- src/components/Page/Home.js
+++ src/components/Page/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Home = () => (
-  <div className="header section content-container">
-    <p className="header__text">Hello, I'm</p>
-    <h1 className="header__title">Aurelien Roux</h1>
-    <h2 className="header__sub">Building things for the web</h2>
-    <p className="header__text header__text--second">
+  <div className="home section content-container">
+    <p className="home__text">Hello, I'm</p>
+    <h1 className="home__title">Aurelien Roux</h1>
+    <h2 className="home__sub">Building things for the web</h2>
+    <p className="home__text home__text--second">
       I'm a Front-End developer based in Montreal,
       who likes to create engaging websites and amazing web applications
     </p>
