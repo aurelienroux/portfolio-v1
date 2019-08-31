32468f07fe42f04678e89c251491f59da63402cf
Aurélien Roux <aureroux13@me.com>
2019-08-31 12:11:22 -0400

    fix header & footer nodes linking in index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import 'typeface-roboto-condensed';
 import 'typeface-montserrat';
 import './styles/styles.scss';
 import * as serviceWorker from './serviceWorker';
-import Header from '../components/Layout/Header';
-import Footer from '../components/Layout/Footer';
+import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
 import AppRouter from './routes/AppRouter';
 
-ReactDOM.render(<Header />, document.getElementById('Header'));
+ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<AppRouter />, document.getElementById('Main'));
-ReactDOM.render(<Footer />, document.getElementById('Footer'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
