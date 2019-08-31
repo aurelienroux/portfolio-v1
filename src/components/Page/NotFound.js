11e6c21df4ce714db0df25d30ebc5d2f9872886b
Aurélien Roux <aureroux13@me.com>
2019-08-31 13:57:25 -0400

    add 404 bg image and fix scss

--- src/components/Page/NotFound.js
+++ src/components/Page/NotFound.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-  <div className="not-found section content-container">
+  <div className="not-found section">
     <div className="title title--small">
-      <h3 className="title__text">
-        404
-      </h3>
+      <div className="title__image"></div>
+      <h3 className="title__text">404</h3>
     </div>
+    <div className="content-container">
     <h4 class="subtitle">It seems you got lost...</h4>
-    <Link to="/">
-      Back to homepage
-    </Link>
+    <Link to="/">Back to homepage</Link>
+    </div>
   </div>
 );
 
