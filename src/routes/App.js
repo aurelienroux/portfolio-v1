48cb3060911ce38902a8e3c59118045c99bfddd9
Aurélien Roux <aureroux13@me.com>
2019-09-01 12:04:31 -0400

    fix router 404 bug

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import About from '../components/Page/About';
 import Contact from '../components/Page/Contact';
 import Home from '../components/Page/Home';
@@ -9,12 +9,13 @@ import NotFound from '../components/Page/NotFound';
 
 const App = () => (
   <Router>
-    <CustomRoute path="/" exact component={Home} />
-    <CustomRoute path="/about" exact component={About} />
-    <CustomRoute path="/projects" exact component={Projects} />
-    <CustomRoute path="/contact" exact component={Contact} />
-    <CustomRoute path="/404" component={NotFound} />
-    <Redirect to="/404" />
+    <Switch>
+      <CustomRoute path="/" exact component={Home} />
+      <CustomRoute path="/about" exact component={About} />
+      <CustomRoute path="/projects" exact component={Projects} />
+      <CustomRoute path="/contact" exact component={Contact} />
+      <CustomRoute path="*" component={NotFound} />
+    </Switch>
   </Router>
 );
 
